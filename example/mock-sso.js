278df55310ad41e9d04e8be0ddc46ea32dfe3518
Mark Higham <mark.higham@gmail.com>
2021-06-23 12:26:22 +0100

    added a simple example app

---
+++ example/mock-sso.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const createApplication = require("./");
+const { AuthorizationCode } = require("simple-oauth2");
+
+createApplication(({ app, callbackUrl }) => {
+  const client = new AuthorizationCode({
+    client: {
+      id: "CLIENT_ID",
+      secret: "CLIENT_SECRET",
+    },
+    auth: {
+      tokenHost: "http://mock-sso:5000",
+      tokenPath: "/o/token",
+      authorizePath: "/o/authorize",
+    },
+  });
+
+  // Authorization uri definition
+  const authorizationUri = client.authorizeURL({
+    redirect_uri: callbackUrl,
+    scope: "notifications",
+    state: "3(#0/!~",
+  });
+
+  // Initial page redirecting to Github
+  app.get("/auth", (req, res) => {
+    console.log(authorizationUri);
+    res.redirect(authorizationUri);
+  });
+
+  // Callback service parsing the authorization token and asking for the access token
+  app.get("/callback", async (req, res) => {
+    const { code } = req.query;
+    const options = {
+      code,
+    };
+
+    try {
+      const accessToken = await client.getToken(options);
+
+      console.log("The resulting token: ", accessToken.token);
+
+      return res.status(200).json(accessToken.token);
+    } catch (error) {
+      console.error("Access Token Error", error.message);
+      return res.status(500).json("Authentication failed");
+    }
+  });
+
+  app.get("/", (req, res) => {
+    res.send('Hello<br><a href="/auth">Log in with http://mock-sso:5000</a>');
+  });
+});
