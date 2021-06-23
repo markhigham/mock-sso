594ec6e18a719b804d42276be826720da543714e
Mark Higham <mark.higham@gmail.com>
2021-06-23 12:20:43 +0100

    for other (none staff-sso) clients create a uuid as token

--- src/lib/routes/token.ts
+++ src/lib/routes/token.ts
@@ -1,4 +1,5 @@
 import { ILogger, LogManager } from "../logger";
+import * as uuid from "uuid";
 
 export class TokenRoutes {
   private logger: ILogger;
@@ -10,7 +11,7 @@ export class TokenRoutes {
     this.logger.info(`${req.method} ${req.originalUrl}`);
     this.logger.debug(req.body);
     res.status(200).send({
-      access_token: req.body.code,
+      access_token: req.body.code || uuid.v4(),
       token_type: "Bearer",
     });
   }
